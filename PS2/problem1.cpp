8611e5bfea5d384eb0f37b46a13d0246026418d7
ZohairZaman <zohairzaman14@gmail.com>
2015-09-22 16:53:46 -0500

    Homework 2

--- PS2/problem1.cpp
+++ PS2/problem1.cpp
@@ -40,16 +40,24 @@ On Windows:
 
 using namespace std;
 
-void printSecondary() {
-    cout << "... but maybe I will make a better one one day?" << endl
+// function prototype
+void PrintSecondary();
+
+int main()
+{
+    for (int i = 0; i < 10; i++)
+        {
+            cout << "I wish I had " << i << " startups." << endl;
+
+            if (i == 3)
+            {
+                cout << "I hate my startup." << endl;
+                PrintSecondary();
+            }
+        }
 }
 
-int main() {
-    for (int i = 0; i < 10; i++) {
-        cout << "I wish I had " << i << " startups." << endl;
-
-        if (i == 3) {
-            cout << "I hate my startup." << endl;
-            PrintSecondary;
-        }
+void PrintSecondary()
+{
+  cout << "... but maybe I will make a better one one day?" << endl;
 }
